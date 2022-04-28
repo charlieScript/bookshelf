1bbbcb56b2e065bc355e466c9a33641403630b9a
charlieScript <gozione3@gmail.com>
2022-04-28 11:44:33 +0100

    added routes for users and books in app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import cors from "cors";
 // Create Express server
 const app: express.Express = express();
 
@@ -7,7 +7,16 @@ const app: express.Express = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+app.use(express.static('./public'));
 
+// auth
+import userRoutes from './routes/user.routes'
+import bookRoutes from './routes/books.routes'
+app.use('/auth', userRoutes)
+app.use('/book', bookRoutes)
+
+app.get('/', (req, res) => res.send('Working now'))
 
 app.use((_req, res): void => {
   res.status(404).send({
